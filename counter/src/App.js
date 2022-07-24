fa9d32847a8aed5d14de421c689ebc6f04e15f20
iamrch <roman.hinds17@gmail.com>
2022-07-24 16:47:07 -0400

    heroku

--- counter/src/App.js
+++ counter/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import io from 'socket.io-client';
 import Home from './Pages/Home/Home';
-export const socket = io.connect("http://localhost:3001");
+export const socket = io.connect("https://mighty-escarpment-43370.herokuapp.com/");
 
 function App() {
   
--- server/index.js
+++ server/index.js
@@ -71,6 +71,6 @@ io.on("connection", (socket) => {
 
 
 
-server.listen(3001, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log("SERVER IS RUNNING");
 })
